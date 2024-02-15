c5386ada69976770f60704fc46c9852d7dbf744d
Vivek Aryan <aryanravula@gmail.com>
2024-02-15 07:55:41 -0600

    Started creating the reddit extraction pipeline.

--- dags/reddit_dag.py
+++ dags/reddit_dag.py
@@ -1,4 +1,6 @@
 from airflow import DAG 
+from airflow.operators.python_operator import PythonOperator
+
 from datetime import datetime
 import os 
 import sys
@@ -9,3 +11,29 @@ default_args = {
     'owner': 'Vivek Aryan Ravula',
     'start_date': datetime(2024,2,14)
 }
+
+file_postfix = datetime.now().strftime("%Y%m%d")
+
+dag = DAG(
+    dag_id = 'etl_reddit_pipeline',
+    default_args = default_args,
+    schedule_intervals = '@daily',
+    catchup = False,
+    tags = ['reddit', 'etl', 'pipeline']
+)
+
+# DAG 1: Extraction from reddit
+
+# Extracting 100 posts from a subreddit in a particular day.
+extract = PythonOperator (
+    task_id = 'reddit_extraction',
+    python_callable = reddit_pipeline,
+    op_kwargs = {
+        'file_name': f'reddit_{file_postfix}',
+        'subreddit': 'LocalLlama',
+        'time_filter': 'day',
+        'limit': 100
+    }
+)
+
+# DAG 2: Uppload to S3
---
+++ pipelines/reddit_pipeline.py
@@ -0,0 +1,8 @@
+def reddit_pipeline(file_name: str, subreddit: str, time_filter = 'day', limit = None):
+    # connecting to reddit instance
+    instance = connect_reddit(CLIENT_ID, SECRET, 'Airscholar Agent')
+    
+
+    # extraction
+    # transformation
+    # loading to csv
