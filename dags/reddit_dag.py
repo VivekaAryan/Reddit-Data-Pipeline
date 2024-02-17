c097c12e28cf7a31613ebd474efa96e9d16debea
Vivek Aryan <aryanravula@gmail.com>
2024-02-17 16:23:09 -0600

    recieved 401 HTTP response

--- dags/reddit_dag.py
+++ dags/reddit_dag.py
@@ -1,42 +1,42 @@
-import os 
+import os
 import sys
 from datetime import datetime
-from airflow import DAG 
 
+from airflow import DAG
 from airflow.operators.python import PythonOperator
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+# from pipelines.aws_s3_pipeline import upload_s3_pipeline
 from pipelines.reddit_pipeline import reddit_pipeline
 
 default_args = {
-    'owner': 'Vivek Aryan Ravula',
+    'owner': 'Vivek Aryan',
     'start_date': datetime(2024, 2, 14)
 }
 
 file_postfix = datetime.now().strftime("%Y%m%d")
 
 dag = DAG(
-    dag_id = 'etl_reddit_pipeline',
-    default_args = default_args,
-    schedule_interval = '@daily',
-    catchup = False,
-    tags = ['reddit', 'etl', 'pipeline']
+    dag_id='etl_reddit_pipeline',
+    default_args=default_args,
+    schedule_interval='@daily',
+    catchup=False,
+    tags=['reddit', 'etl', 'pipeline']
 )
 
 # DAG 1: Extraction from reddit
-
-# Extracting 100 posts from a subreddit in a particular day.
-extract = PythonOperator (
-    task_id = 'reddit_extraction',
-    python_callable = reddit_pipeline,
-    op_kwargs = {
+# extraction from reddit
+extract = PythonOperator(
+    task_id='reddit_extraction',
+    python_callable=reddit_pipeline,
+    op_kwargs={
         'file_name': f'reddit_{file_postfix}',
         'subreddit': 'dataengineering',
         'time_filter': 'day',
         'limit': 100
     },
-    dag = dag
+    dag=dag
 )
 
 # DAG 2: Uppload to S3
