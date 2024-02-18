07810783c2e106b2c338f7c1f15163f45f875ec9
Vivek Aryan <aryanravula@gmail.com>
2024-02-18 08:31:26 -0600

    Setting up aws pipeline and pushing the data into AWS S3 bucket.

--- dags/reddit_dag.py
+++ dags/reddit_dag.py
@@ -4,10 +4,9 @@ from datetime import datetime
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-# from pipelines.aws_s3_pipeline import upload_s3_pipeline
+from pipelines.aws_s3_pipeline import upload_s3_pipeline
 from pipelines.reddit_pipeline import reddit_pipeline
 
 default_args = {
@@ -40,3 +39,11 @@ extract = PythonOperator(
 )
 
 # DAG 2: Uppload to S3
+
+upload_s3 = PythonOperator(
+    task_id = 's3_upload',
+    python_callable = upload_s3_pipeline,
+    dag = dag
+)
+
+extract >> upload_s3
